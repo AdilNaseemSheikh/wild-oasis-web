72682285880968d9b6fa7ebb8143c54f1c0fa0bc
adilnaseemsheikh <adilnaseemsheikh@gmail.com>
2024-07-19 21:49:24 +0500

    basic layout added

--- app/page.js
+++ app/page.js
@@ -1,5 +1,12 @@
+import Link from "next/link";
+import Navigation from "./components/Navigation";
+
+
+
 export default function Home() {
   return (
-    <h1>Hello Next!</h1>
+    <>
+      <h1>The Wild Oasis. Welcome to Paradise!</h1>
+    </>
   );
 }
